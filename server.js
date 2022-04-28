9a45e87c525324b3e3d5ae71486d9a0332ae45c6
callanhunter <callan57hunter@gmail.com>
2022-04-28 16:13:33 -0700

    add get request to return saved notes

--- server.js
+++ server.js
@@ -1,33 +1,39 @@
 const express = require('express');
 const path = require('path');
+let db = require('./db/db.json');
+
+
+const uuid = require('./helpers/uuid');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// The following HTML routes should be created:
-    // GET /notes should return the notes.html file.
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+app.get('/api/notes', (req, res) => {
+    console.log(notes);
+    res.json(notes);
+        res.json(`${req.method} request received to get reviews`);
+        fs.readFile('./db/db.json', 'utf8', (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                res.json(JSON.parse(data));
+            }
+        }
+    console.info(`${req.method} request received to get reviews`);
+});
+
+
+// return to the notes.html file.
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-    // GET * should return the index.html file.
+// return to the index.html file.
 app.get('*', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
-
-
-
-
-
-
-// The following API routes should be created:
-    // GET /api/notes should read the db.json file and return all saved notes as JSON.
-
-    // POST /api/notes should receive a new note to save on the request body, 
-    // add it to the db.json file, and then return the new note to the client. 
-    // You'll need to find a way to give each note a unique id when it's saved 
-    // (look into npm packages that could do this for you).
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
